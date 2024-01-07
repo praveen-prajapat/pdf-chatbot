97acc90fce46fb879cb1eeda7fb85b716e52feaf
praveen-prajapat <128595854+praveenprajapat2004@users.noreply.github.com>
2024-01-07 18:16:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers   #we can also use LLamaCPP
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationalBufferMemory
+from langchain.document_loaders import PyPDFLoader
+import os
+import tempfile
+
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! ask me anything about"]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["hey!"]
+
+def conversation_chat(query,chain,history):
+    result = chain({"question":query,"chat_history":history})
+    history.append((query,result["answer"]))
+    return result["answer"]
+
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form',clear_on_submit=True)
+            user_input = st.text_input("Question:",placeholder="ask about your pdf")
+            submit_button = st.form_submit_button(label='send')
+        if submit_button and user_input:
+            with st.spinner('generating response...'):
+                output=conversation_chat(user_input,chain,st.session_state['history'])
+
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state['past'][i],is_user=True,key=str(i)+'_user',avatar_style="thumbs")
+                message(st.session_state["generated"][i],key=str(i),avatar_style="fun-emoji")
+
+def create_conversational_chain(vector_store):
+    llm=LlamaCpp(
+        streaming=True,
+        model_path="",
+        temperature=0.75,
+        top_p=1,
+        verbose=True,
+        n_ctx=4096
+    )
+    memory= ConversationalBufferMemory(memory_key="chat_history",return_messages=True)
+    
+    chain = ConversationalRetrievalChain.form_llm(llm=llm,chain_type='stuff',
+                                                  retriever=vector_store.as_retriever(search_kwargs={"k":2}),
+                                                  memory=memory)
+    return chain
+
+def main():
+    #initialize session state
+    initialize_session_state()
+    st.title("multi-PDF chatbot")
+
+    #initialize streamlit
+    st.sidebar.title("document processing")
+    uploaded_files = st.sidebar.file_uploader("Upload files",accept_multiple_files=True)
+
+    if uploaded_files:
+        text=[]
+        for file in uploaded_files:
+            file_extension = os.path.splitext(file.name)[1]
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(file.read())
+                temp_file_path = temp_file.name
+
+            loader=None
+            if file_extension ==".pdf":
+                loader=PyPDFLoader(temp_file_path)
+
+            if loader:
+                text.extend(loader.load())
+                os.remove(temp_file_path)
+
+        text_splitter =RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=20)
+        text_chunks=text_splitter.split_documents(text)
+
+        #create embeddings
+        embeddings= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                          model_kwargs={'device':'cpu'})
+        
+        #create vector store
+        vector_store =FAISS.from_documents(text_chunks,embedding=embeddings)
+
+        #create the chain object
+        chain = create_conversational_chain(vector_store)
+
+        display_chat_history(chain)
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
+
